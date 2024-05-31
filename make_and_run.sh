a407d3991a7d19686655bd43ded236c0de91c797
SiyuanWang <12012324@mail.sustech.edu.cn.com>
2024-05-31 21:18:53 +0800

    update this make&run script

--- make_and_run.sh
+++ make_and_run.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+
+# Check if "nabo_core/build" and "nabo_mujoco/build" exist
+# ======================================================== #
+if [ ! -d "nabo_core/build" ]; then
+  mkdir -p nabo_core/build
+fi
+
+if [ ! -d "nabo_mujoco/build" ]; then
+  mkdir -p nabo_mujoco/build
+fi
+# ======================================================== #
+
+
 cd ./nabo_core/build
 cmake ..
 make -j12
